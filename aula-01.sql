13ece46937b57dc6423f555378e1ec9450928519
Inaldo Anjos <inaldoanjosjr@gmail.com>
2022-09-21 21:58:16 -0300

    Primeira aula de SQL

---
+++ aula-01.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE alura;
+
+CREATE TABLE aluno(id SERIAL,
+                   nome VARCHAR(255),
+                   cpf CHAR(11),
+                   observacao TEXT,
+                   idade INTEGER,
+                   mensalidade NUMERIC(10,2),
+                   altura REAL, 
+                   ativo BOOLEAN, 
+                   data_nascimento DATE,
+                   hora_aula TIME,
+                   matriculado_em TIMESTAMP);
+
+SELECT * FROM aluno;
